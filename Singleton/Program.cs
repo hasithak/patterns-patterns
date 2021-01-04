c6082996dc4bf9e0c90378cc98ab836ea9eea915
HasithaD <hasithad@irononetech.com>
2021-01-04 19:57:04 +0530

    Added singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var instance = Singleton.SingleInstance;
+            instance.Value = "value set :D";
+            instance.PrintDetails();
+
+            var instance2 = Singleton.SingleInstance;
+            instance2.PrintDetails();
+
+            var instance3 = Singleton.SingleInstance;
+            instance3.PrintDetails();
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        private static Singleton instance = null;
+        public string Value { get; set; }
+        private Singleton()
+        {
+        }
+
+        public static Singleton SingleInstance
+        {
+            get
+            {
+                if (instance == null) 
+                { 
+                    instance = new Singleton();
+                }
+
+                return instance;
+
+            }
+        }
+
+        public void PrintDetails()
+        {
+            Console.WriteLine($"value is \"{Value}\"");
+        }
+    }
+}
